e53ae2c50d05d9b34644ceef7aef7608e01e7ce8
Aman-codde <amandeepk221@gmail.com>
2022-11-11 07:36:04 -0800

    feat(): add   BigO(n) and BigO(1)

---
+++ Scalability_2.js
@@ -0,0 +1,8 @@
+// O(1) -- Constant time
+const boxes = new Array(100).fill('Box');
+
+function compressBoxes(boxes) {
+    console.log(boxes[0]);
+}
+
+compressBoxes(boxes);// O(1) -- it will take 1 step to complete

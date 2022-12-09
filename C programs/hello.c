157ae931ed45a3ee1d818ddfa022f6654203e213
Aman-codde <amandeepk221@gmail.com>
2022-12-09 08:22:38 -0800

    feat: add c program

---
+++ C programs/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    char ch;
+    scanf("Enter character %c",&ch);
+    printf("Character %c",ch);
+    return 0;
+}
